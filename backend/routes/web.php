4fbf11ecdf941f62aaa406ba41b60464b6da7ff9
Shubhanjal Sharma <148334862+shubhanjal44@users.noreply.github.com>
2026-06-02 23:26:03 +0530

    Run migrations on startup

---
+++ backend/routes/web.php
@@ -0,0 +1,8 @@
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/setup-db', function () {
+    Artisan::call('migrate', ['--force' => true]);
+    Artisan::call('db:seed', ['--force' => true]);
+
+    return 'Database setup completed!';
+});
